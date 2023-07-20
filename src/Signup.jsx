b13b7d556e769cbea0ff1d73736fd64f3d08f518
Aryann15 <rajlysm15@gmail.com>
2023-07-21 01:07:19 +0530

    some basic css

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
+import './Signup.css'
 
 const Signup = () => {
   return (
-    <div>
-       Username = <input type="text" />
+    <div className='container'>
+        <div className="centered-element">
+       Username = <input type="text" style={{
+        padding: 5,
+        borderRadius: 8,
+    boxShadow:" 0 4 8 rgba(0, 0, 0, 0.1)"}} />
        <br />
-       Password = <input type="text" />
+       Password = <input type="text" style={{
+        padding: 5,
+        borderRadius: 8,
+    boxShadow:" 0 4 8 rgba(0, 0, 0, 0.1)"}} />
+       
        <br />
-       <button>Sign up</button>
-    </div>
+       <button style={{
+        padding: 10,
+        
+        borderRadius: 8,
+    boxShadow:" 0 4 8 rgba(0, 0, 0, 0.1)"}}>Sign up</button>
+       </div></div>
   )
 }
 
