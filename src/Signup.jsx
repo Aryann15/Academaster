63f94d0e857bf9a7811621cef6c13b61749293b8
Aryann15 <rajlysm15@gmail.com>
2023-07-21 00:42:58 +0530

    starting point for signup page

---
+++ src/Signup.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+       Username = <input type="text" />
+       <br />
+       Password = <input type="text" />
+       <br />
+       <button>Sign up</button>
+    </div>
+  )
+}
+
+export default Signup
