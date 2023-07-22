168dc9fb021ceb05c46196d857d5447b91f1b45f
Aryann15 <rajlysm15@gmail.com>
2023-07-22 12:32:46 +0530

    signup page

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import './Signup.css'
-import { TextField } from '@mui/material'
-import Button from '@mui/material/Button'
+import React from "react";
+import "./Signup.css";
+import { TextField, Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
 const Signup = () => {
   return (
-    <div> <center>
-        <div style={{marginTop:150,
-        marginBottom:10}}>
-Welcome to Academaster! Sign Up Below!
-        </div>
+    <div>
+      {" "}
+      <center>
+        <div style={{ paddingTop: 150, marginBottom: 10 }}>
+            <Typography variant={'h6'}>
+          Welcome to Academaster! Sign Up Below!
+          </Typography></div>
+      </center>
+      <center>
+      <Card varint="outlined" style={{width:400 , padding: 20}}>
+          <TextField fullWidth={true} id="outlined-basic" label="Username" variant="outlined" />
+          <br /> <br />
+          <TextField fullWidth={true} id="outlined-basic" label="Password" variant="outlined" />
+          <br />
+          <br /><br />
+          <Button size="large" variant="contained"> Sign Up</Button>
+          </Card>
+      </center>
+    </div>
+  );
+};
 
-</center>
-<center>
-
-
-    <div style={{
-        border: '2px solid black',
-        width:400,
-        marginTop: 150
-    }}>
-        
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-       <br />
-       Password = <input type="text" ></input>
-       <br />
-       <Button variant='contained'> Sign Up</Button>
-       </div></center></div> 
-  )
-}
-
-export default Signup
+export default Signup;
