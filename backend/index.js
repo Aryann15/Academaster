fcd778534430655bbcde91950cfcf3ce5a54dfdb
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-06 20:01:34 +0530

    admin routes

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,28 @@ const app = express()
 
 app.use(express.json)
 
+let ADMINS =[]
+let USERS = []
+let COURSES = []
+
+//admin routes
+
+app.post('/admin/signup',(req,res) => {
+
+})
+app.post('/admin/login',(req,res) => {
+    
+})
+app.post('/admin/courses',(req,res) => {
+    
+})
+app.put('/admin/courses/:courseId',(req,res) => {
+    
+})
+app.get('/admin/courses',(req,res) => {
+})
+
+
 app.listen(3000,() => {
     console.log('Server is running on port 3000');
 })
