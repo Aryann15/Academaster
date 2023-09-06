38522ac6e05c0ff9d5f31617a34f36d312bc045d
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-06 20:09:50 +0530

    admin signup route

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,39 @@
-const express = require ('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.json)
+app.use(express.json);
 
-let ADMINS =[]
-let USERS = []
-let COURSES = []
+let ADMINS = [];
+let USERS = [];
+let COURSES = [];
 
 //admin routes
 
-app.post('/admin/signup',(req,res) => {
-
-})
-app.post('/admin/login',(req,res) => {
+app.post("/admin/signup", (req, res) => {
+  const admin = req.body;
+  const existingAdmin = ADMINS.find((a) => a.username === admin.username);
+  if (existingAdmin) {
+    res.status(403).json({ message: "admin already exists" });
+  } else {
+    ADMINS.push(admin);
+    res.json({ message: " Admin created succesfully" });
+  }
+});
+app.post("/admin/login", (req, res) => {
     
-})
-app.post('/admin/courses',(req,res) => {
-    
-})
-app.put('/admin/courses/:courseId',(req,res) => {
-    
-})
-app.get('/admin/courses',(req,res) => {
-})
+});
+app.post("/admin/courses", (req, res) => {});
+app.put("/admin/courses/:courseId", (req, res) => {});
+app.get("/admin/courses", (req, res) => {});
 
 //user routes
 
-app.post('/user/signup',(req,res) => {
+app.post("/user/signup", (req, res) => {});
+app.post("/user/login", (req, res) => {});
+app.get("/user/courses", (req, res) => {});
+app.post("/user/courses/:courseId", (req, res) => {});
+app.get("/user/purchasedCourses", (req, res) => {});
 
-})
-app.post('/user/login',(req,res) => {
-    
-})
-app.get('/user/courses',(req,res) => {
-    
-})
-app.post('/user/courses/:courseId',(req,res) => {
-    
-})
-app.get('/user/purchasedCourses',(req,res) => {
-})
-
-app.listen(3000,() => {
-    console.log('Server is running on port 3000');
-})
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
