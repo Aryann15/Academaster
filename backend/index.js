dcd16ac50aa444669d7eaa7e7ff4854beacd6063
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-12 18:21:15 +0530

    admin courses routes updated

--- backend/index.js
+++ backend/index.js
@@ -88,24 +88,23 @@ app.post("/admin/login", async (req, res) => {
 
 app.post("/admin/courses",authenticateJwt, async (req, res) => {
   const course = new Course(req.body);
-  await course.save();
   course.id = Date.now(); //use timestamp as course ID
+  await course.save();
   res.json({ message: "Course created successfully", courseId: course.id });
 });
 
-app.put("/admin/courses/:courseId", authenticateJwt, (req, res) => {
-  const courseId = Number(req.params.courseId);
-  const course = COURSES.find((c) => c.id === courseId);
+app.put("/admin/courses/:courseId", authenticateJwt, async (req, res) => {
+  const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, {new:true})
   if (course) {
-    Object.assign(course, req.body);
     res.json({ message: "Course updated successfully" });
   } else {
     res.status(404).json({ message: "Course not found" });
   }
 });
 
-app.get("/admin/courses",authenticateJwt , (req, res) => {
-  res.json({ courses: COURSES });
+app.get("/admin/courses",authenticateJwt ,async (req, res) => {
+  const courses = await Course.find({})
+  res.json({ courses });
 });
 //user routes
 
