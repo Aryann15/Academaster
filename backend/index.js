2fbed8436f47e83d2419a7cea1092b71cc0d892b
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-12 18:34:32 +0530

    user signup and login routes updated

--- backend/index.js
+++ backend/index.js
@@ -106,24 +106,27 @@ app.get("/admin/courses",authenticateJwt ,async (req, res) => {
   const courses = await Course.find({})
   res.json({ courses });
 });
+
 //user routes
 
-app.post("/user/signup", (req, res) => {
-  const user = req.body;
-  const existingUser = USERS.find((a) => a.username === user.username);
-  if (existingUser) {
-    res.status(403).json({ message: "Someone with that username already exists" });
+app.post("/user/signup", async (req, res) => {
+  const {username, password} = req.body;
+  const user = await User.findOne({username})
+  if (user) {
+    res.status(403).json({ message:"user already exists"});
   } else {
-    const token = generateJwt(user);
-    USERS.push(user);
+    const newUser = new User({username , password })
+    await newUser.save()
+    const token = jwt.sign({username, role:'user'},secretKey,{expiresIn : '1h'})
     res.json({ message: "User created successfully",token });
   }
 });
-app.post("/user/login",(req, res) => {
+
+app.post("/user/login",async (req, res) => {
   const {username,password} =req.headers;
-  const user = USERS.find(u => u.username == username && u.password ==password)
+  const user = await User.findOne({username,password})
   if (user){
-    const token = generateJwt(user);
+    const token = jwt.sign ({username, role: 'user'} ,secretKey , {expiresIn: '1h'});
     res.json({ message: "Logged in successfully",token});
   }
   else{
@@ -131,13 +134,13 @@ app.post("/user/login",(req, res) => {
   }  
 });
 
-app.get("/user/courses",authenticateJwt, (req, res) => {
-  res.json({ courses: COURSES });
+app.get("/user/courses",authenticateJwt, async (req, res) => {
+  const courses = await Course.find({published: true})
 });
 
-app.post("/user/courses/:courseId",authenticateJwt, (req, res) => {
-  const courseId = Number(req.params.courseId);
-  const course = COURSES.find((c) => c.id === courseId );
+app.post("/user/courses/:courseId",authenticateJwt, async (req, res) => {
+  
+  const course = await Course.findById(req.params.courseId)
   if (course) {
     const user = USERS.find(u => u.username === req.user.username);
     if (user){
