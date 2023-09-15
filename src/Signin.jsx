30c2ceedfb66d92ca93cf42fc21b3019365d0943
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-15 08:03:24 +0530

    sedning requests to the backend

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./Signin.css";
 import { TextField, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
@@ -7,27 +6,57 @@ const Signin = () => {
   return (
     <div>
       {" "}
-      <div style={{
-        display: "flex", 
-        justifyContent: "center",
-        paddingTop: 150,
-        marginBottom: 10
-      }}>
-        
-            <Typography variant={'h6'}>
-          Welcome to Academaster! Sign Up Below!
-          </Typography></div>
-      
-      <div style={{display: "flex", 
-        justifyContent: "center",}}>
-      <Card varint="outlined" style={{width:400 , padding: 20}}>
-          <TextField fullWidth={true} id="outlined-basic" label="Username" variant="outlined" />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          paddingTop: 150,
+          marginBottom: 10,
+        }}
+      >
+        <Typography variant={"h6"}>Welcome Back ! Sign in Below!</Typography>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Card varint="outlined" style={{ width: 400, padding: 20 }}>
+          <TextField
+            fullWidth={true}
+            id={"username"}
+            label="Username"
+            variant="outlined"
+          />
           <br /> <br />
-          <TextField fullWidth={true} id="outlined-basic" label="Password" variant="outlined" type="passwordcdc"/>
+          <TextField
+            fullWidth={true}
+            id={"password"}
+            label="Password"
+            variant="outlined"
+            type={"password"}
+          />
           <br />
-          <br /><br />
-          <Button size="large" variant="contained"> Sign Up</Button>
-          </Card>
+          <br />
+          <br />
+          <Button
+            onClick={() => {
+              let username = document.getElementById("username").value;
+              let password = document.getElementById("password").value;
+              fetch("http://localhost:3000/admin/signup", {
+                method: "POST",
+                body: JSON.stringify({
+                  username,
+                  password,
+                }),
+                headers: {
+                  "Content-type" : "application/json" 
+                }
+              });
+            }}
+            size="large"
+            variant="contained"
+          >
+            {" "}
+            Sign in
+          </Button>
+        </Card>
       </div>
     </div>
   );
