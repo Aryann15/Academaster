db404ffd0ed5a5287a8d187700d30151bc8f86a2
Raj Aryan Singh <rajlysm15@gmail.com>
2023-09-17 17:51:52 +0530

    using useNav. hook

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,24 +1,41 @@
 import React from "react";
 import Button from "@mui/material/Button";
 import { Typography } from "@mui/material";
-
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div style={{
-      display: "flex",
-      justifyContent: "space-between",
-      padding: 10
-    }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        padding: 10,
+      }}
+    >
       <div>
         <Typography variant={"h6"}> Academaster </Typography>
       </div>
-      <div style={{display:"flex"}}>
-        <div style={{marginRight:10}}>
-        <Button variant="contained">Sign in</Button>
+      <div style={{ display: "flex" }}>
+        <div style={{ marginRight: 10 }}>
+          <Button
+            onClick={() => {
+              navigate("/signin");
+            }}
+            variant="contained"
+          >
+            Sign in
+          </Button>
         </div>
         <div>
-        <Button variant="contained">Sign up</Button>
+          <Button
+            onClick={() => {
+              navigate("/signup");
+            }}
+            variant="contained"
+          >
+            Sign up
+          </Button>
         </div>
       </div>
     </div>
